151e814dbc18d181c4df7ef14b09b5332118197c
Sjbrimley26 <sjbrimley26@live.com>
2018-05-17 23:37:19 -0700

    I figured out how to do setInterval

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,55 @@
 import Html exposing ( Html, div, text )
 import Html.Attributes exposing ( class )
+import Time exposing ( Time, second, millisecond, every )
 import Global_CSS exposing ( flex, justify_center )
 
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Html.program 
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
 type alias Model =
   { greeting: String
+  , counter : Int
   }
 
 model : Model
 model =
   { greeting = "Hello World!"
+  , counter = 0
   }
 
-type Msg = Default
+type Msg =
+  ChangeGreeting
 
-update : Msg -> Model -> Model
+init : ( Model, Cmd Msg )
+init = 
+  ( model, Cmd.none )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  model
+  case msg of
+    ChangeGreeting ->
+      if model.counter < 3 then
+        case model.counter of
+          0 ->
+            ({ model | counter = model.counter + 1, greeting = "Hello World!" }, Cmd.none )
+          1 ->
+            ({ model | counter = model.counter + 1, greeting = "Welcome In!" }, Cmd.none )
+          2 ->
+            ({ model | counter = model.counter + 1, greeting = "How's it Hangin'?" }, Cmd.none )
+          _ ->
+            ({ model | counter = model.counter + 1, greeting = "Hello World!" }, Cmd.none )
+      else
+        ({ model | counter = 0, greeting = "What a great day!" }, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch [ every second changeLoop ]
 
 view : Model -> Html Msg
 view model =
@@ -27,4 +57,7 @@ view model =
     [ div [ class "headerDiv" ] [] 
     , div [ flex, justify_center ] [ text model.greeting ]
     ]
-  
+
+changeLoop : Time -> Msg
+changeLoop time =
+  ChangeGreeting
